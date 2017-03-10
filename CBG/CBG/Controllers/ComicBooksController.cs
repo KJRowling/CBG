b1f470da6150e5c8d475a1ab7af386fe27f4996f
KJRowling <lickthawrapper@gmail.com>
2017-03-10 02:18:25 -0500

    Added comic books controller

---
+++ CBG/CBG/Controllers/ComicBooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBG.Controllers
+
+{
+    //inherit from controller class
+    public class ComicBooksController : Controller
+    {
+
+
+    }
+}
---
+++ CBG/CBG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CBG.MvcApplication" Language="C#" %>
