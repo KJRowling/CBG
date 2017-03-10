ab9b8496d600b997c670d1a41e82409e80d0af8e
KJRowling <lickthawrapper@gmail.com>
2017-03-10 02:21:21 -0500

    Added method detail to comicbookscontroller

--- CBG/CBG/Controllers/ComicBooksController.cs
+++ CBG/CBG/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace CBG.Controllers
 
 {
     //inherit from controller class
+    //controller classes must be public
     public class ComicBooksController : Controller
     {
-
+        //every public method on a controller is an "action method"
+        //can't be void??
+        //must be public or mvc won't be able to find and call
+        public string Detail()
+        {
+            return "Hello";
+        }
 
     }
 }
