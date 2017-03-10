3891263af92ed698999d8cdc096e8f1f32853535
KJRowling <lickthawrapper@gmail.com>
2017-03-10 15:43:33 -0500

    Updated detail action method to use actionresult

--- CBG/CBG/Controllers/ComicBooksController.cs
+++ CBG/CBG/Controllers/ComicBooksController.cs
@@ -14,9 +14,25 @@ namespace CBG.Controllers
         //every public method on a controller is an "action method"
         //can't be void??
         //must be public or mvc won't be able to find and call
-        public string Detail()
+
+            //actionresult almost always used for return type of action methods
+        public ActionResult Detail()
         {
-            return "Hello";
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                
+                    return Redirect("/"); //Return string for target URL
+
+            }
+
+            return Content("Hello");
+
+        }
+
+        public string Index()
+        {
+            return "index";
         }
 
     }
